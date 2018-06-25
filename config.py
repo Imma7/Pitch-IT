6dc38946639b07b9755541a7697102e7331fc927
Immanuel Mugambi <immamugambi@gmail.com>
2018-06-25 11:39:13 +0300

    Setting up SQLAlchemy

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://imma:1234@localhost/pitchit'
+
 
 class ProdConfig(Config):
     pass
