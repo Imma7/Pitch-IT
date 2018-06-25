b35ae42701cf401b58a3b99befae4048448d75d3
Immanuel Mugambi <immamugambi@gmail.com>
2018-06-25 15:40:11 +0300

    Registration route

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,7 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
+from ..models import User
+from .forms import RegistrationForm
+from ..db
 from . import auth
 from flask_login import login_user, logout_user, login_required
 
@@ -11,3 +14,15 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
+
+@auth.route('/register', methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html', registration_form = form)
