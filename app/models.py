195cb7c140aa50c84b47fdc731f1f9230944c47f
Immanuel Mugambi <immamugambi@gmail.com>
2018-06-25 11:41:58 +0300

    Created Pitch class

--- app/models.py
+++ app/models.py
@@ -12,3 +12,13 @@ class User(db.Model):
     def __repr__(self):
         return f'User {self.username}'
 
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255))
+    comment = db.Column(db.String)
+    category = db.Column(db.String)
+    vote = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments = db.relationship('Comment', backref = 'pitches', lazy = "dynamic")
+    votes = db.relationship('Vote', backref = 'pitches', lazy = "dynamic")
