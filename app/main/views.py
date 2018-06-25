bca063ecb6f3ef4a4d9f5828773438689d59c25d
Immanuel Mugambi <immamugambi@gmail.com>
2018-06-25 12:16:50 +0300

    Creating Relationships

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..models import Movie, Review
+from ..models import User, Pitch, Category, Vote, Comment
 #Views
 @main.route('/')
 def index():
