8f48cabd001cd828c9833589bb3216bccfa64b47
Immanuel Mugambi <immamugambi@gmail.com>
2018-06-25 15:16:35 +0300

    Logout Authentication route

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,3 +6,8 @@ from flask_login import login_user, logout_user, login_required
 def login():
     return render_template('auth/login.html')
 
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
