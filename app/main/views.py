43fbbbfe743b87360074616b89f91ae11f4cb8f7
Immanuel Mugambi <immamugambi@gmail.com>
2018-06-25 12:50:20 +0300

    User Authentication

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template,request,redirect,url_for
 from . import main
 from ..models import User, Pitch, Category, Vote, Comment
+
 #Views
 @main.route('/')
 def index():
--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,7 @@
 import unittest
-from app.models import Movie
+from app.models import User
 
-class MovieTest(unittest.TestCase):
+class UserTest(unittest.TestCase):
     '''
     Test Class to test the behaviour of the Movie class
     '''
@@ -10,7 +10,22 @@ class MovieTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993)
+        self.new_user = User('imma', 1234)
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_movie,Movie))
+        self.assertTrue(isinstance(self.new_user,User))
+
+class UserModelTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.new_user = User(password = '1234')
+
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.pass_secure is not None)
+
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('1234'))
