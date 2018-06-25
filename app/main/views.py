6b23763b795c16df23c076b044993c066ac8c31b
Immanuel Mugambi <immamugambi@gmail.com>
2018-06-25 14:58:42 +0300

    Flask Login

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,11 @@
 from flask import render_template,request,redirect,url_for
 from . import main
 from ..models import User, Pitch, Category, Vote, Comment
+from flask_login import login_required
 
 #Views
 @main.route('/')
+@login_required
 def index():
     '''
     '''
