66775135173de80ec7e1d423f62b414421097157
Immanuel Mugambi <immamugambi@gmail.com>
2018-06-26 10:12:15 +0300

    Created edit profile page

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,7 @@ from flask import render_template,request,redirect,url_for
 from . import main
 from ..models import User, Pitch, Category, Vote, Comment
 from flask_login import login_required
+from .forms import UpdateProfile
 
 #Views
 @main.route('/')
@@ -12,10 +13,21 @@ def index():
     pass
 
 @main.route('/user/<uname>')
+@login_required
 def profile(uname):
     user = User.query.filter_by(username = uname).first()
 
     if user is None:
         abort(404)
+    
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname = user.username))
 
     return render_template("profile/profile.html", user = user)
