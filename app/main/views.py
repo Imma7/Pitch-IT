0178969825904df7104613b05640ef940d244898
Immanuel Mugambi <immamugambi@gmail.com>
2018-06-26 10:37:06 +0300

    Created iamge Uploads

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,7 @@ from . import main
 from ..models import User, Pitch, Category, Vote, Comment
 from flask_login import login_required
 from .forms import UpdateProfile
+from .. import db, photos
 
 #Views
 @main.route('/')
@@ -12,7 +13,7 @@ def index():
     '''
     pass
 
-@main.route('/user/<uname>')
+@main.route('/user/<uname>/update/pic', methods = ['POST'])
 @login_required
 def profile(uname):
     user = User.query.filter_by(username = uname).first()
@@ -30,4 +31,11 @@ def profile(uname):
 
         return redirect(url_for('.profile', uname = user.username))
 
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+    
     return render_template("profile/profile.html", user = user)
