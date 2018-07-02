aecc26c8d6f83eadfb9d9d26769d424d6274e3b4
Immanuel Mugambi <immamugambi@gmail.com>
2018-07-02 15:17:11 +0300

    Created Comment Form

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,7 +6,14 @@ class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
 
+#Pitch Form
 class PitchForm(FlaskForm):
     category_id = SelectField('Pick A Category', choices=[('1', 'Sales'), ('2', 'Product'), ('3', 'Secrets'), ('4', ('Confessions'), ('5', 'Reviews'))])
     comment = TextAreaField('Comments')
     submit = SubmitField('Submit Commennt')
+
+#Comment Form
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[Required()])
+    submit = SubmitField()
+    vote=RadioField('default field arguments', choices=[('1', 'UpVote'), ('1', 'DownVote')])
