8ae8749e1b28b6876ab312fec51390b5f63455f1
Immanuel Mugambi <immamugambi@gmail.com>
2018-07-03 17:23:37 +0300

    deploying to heroku

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://imma:1234@localhost/pitchit'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
